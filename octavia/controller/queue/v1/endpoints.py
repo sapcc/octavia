e62711c242fd56d6b3aa81cb9743986d6efad2f0
Benjamin Ludwig <benjamin.ludwig@sap.com>
2021-10-25 15:36:34 +0200

    Rename F5 LB rescheduling endpoints
    These endpoints are for F5 PD rescheduling functionality, which can be found here:
    https://github.com/sapcc/octavia-f5-provider-driver/tree/rescheduling
    
    This is to be more technically precise and to avoid confusion with the
    F5 PD 'migration mode', which is for migrating from neutron lbaas.

--- octavia/controller/queue/v1/endpoints.py
+++ octavia/controller/queue/v1/endpoints.py
@@ -60,15 +60,15 @@ class Endpoints(object):
                  load_balancer_id)
         self.worker.failover_loadbalancer(load_balancer_id)
 
-    def migrate_load_balancer(self, context, load_balancer_id, target_host):
-        LOG.info('Migrating load balancer %s to host \'%s\'...',
+    def reschedule_load_balancer(self, context, load_balancer_id, target_host):
+        LOG.info('Rescheduling load balancer %s to host \'%s\'...',
                  load_balancer_id, target_host)
-        self.worker.migrate_loadbalancer(load_balancer_id, target_host)
+        self.worker.reschedule_loadbalancer(load_balancer_id, target_host)
 
-    def migrate_load_balancers(self, context, source_host, target_host):
-        LOG.info('Migrating load balancers from host \'%s\' to host \'%s\'...',
+    def reschedule_load_balancers(self, context, source_host, target_host):
+        LOG.info('Rescheduling load balancers from host \'%s\' to host \'%s\'...',
                  source_host, target_host)
-        self.worker.migrate_loadbalancers(source_host, target_host)
+        self.worker.reschedule_loadbalancers(source_host, target_host)
 
     def failover_amphora(self, context, amphora_id):
         LOG.info('Failing over amphora \'%s\'...',
