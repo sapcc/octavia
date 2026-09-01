a1ea38fcb03c9489d9f05aee85bad1d493f460d7
Benjamin Ludwig <56023005+BenjaminLudwigSAP@users.noreply.github.com>
2026-09-01 10:58:26 +0200

    Add policy rule for CPBMU endpoint (#66) (#67)
    * Add policy rule for CPBMU endpoint
    
    Calling the CPBMU endpoint (in code CrossPoolMembersController.put())
    now does a policy check for put_cross_pool_members. By default, this is
    now admin only and can be overridden via policy file, like this:
    
    "os_load-balancer_api:member:put_cross_pool_members": "role:let_me_cpbmu"
    
    * CPBMU endpoint: Do policy check against LB project
    
    ---------
    
    Co-authored-by: Sebastian Lohff <seba@someserver.de>

--- octavia/api/v2/controllers/member.py
+++ octavia/api/v2/controllers/member.py
@@ -616,6 +616,8 @@ class CrossPoolMembersController(MembersController):
             project_id, provider = db_lb.project_id, db_lb.provider
 
         # Check POST+PUT+DELETE since this operation is all of 'CUD'
+        self._auth_validate_action(context, project_id,
+                                   constants.RBAC_PUT_CROSS_POOL_MEMBERS)
         self._auth_validate_action(context, project_id, constants.RBAC_POST)
         self._auth_validate_action(context, project_id, constants.RBAC_PUT)
         if not additive_only:
