ec73af51bcc9046fb6e8fd19006dd48a3be29a19
Andrew Karpow <andrew.karpow@sap.com>
2026-09-23 09:09:13 -0400

    BMU: Skip subnet validation tests disabled for CC

--- octavia/tests/functional/api/v2/test_member.py
+++ octavia/tests/functional/api/v2/test_member.py
@@ -788,6 +788,7 @@ class TestMember(base.BaseAPITest):
                                               mock_driver.member_batch_update,
                                               self.pool_id, provider_members)
 
+    @testtools.skip('Disabled For CC')
     def test_create_batch_members_with_bad_subnet(self):
         subnet_id = uuidutils.generate_uuid()
         member5 = {'address': '10.0.0.5',
@@ -999,6 +1000,7 @@ class TestMember(base.BaseAPITest):
 
     @mock.patch('octavia.api.drivers.driver_factory.get_driver')
     @mock.patch('octavia.api.drivers.utils.call_provider')
+    @testtools.skip('Disabled For CC')
     def test_update_members_subnet_not_found(
             self, mock_provider, mock_get_driver):
         mock_driver = mock.MagicMock()
